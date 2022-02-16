3a4807cce87286f045e90378f269ffe7361132a6
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-16 09:58:52 +0800

    调整

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 
 
-
 from asyncio import sleep
 from random import random
 import os
 import time
 
+WEIGHT = 30
+HEIGHT = 13
 
 def initGameMap(weight, height):
   gameMap = [];
   for y in range(0, height):
     gameMap.append([])
     for x in range(0, weight):
-      if x == 0 or x == weight - 1 or y ==0 or y == height - 1:
+      if xyIsWall(x, y, weight, height):
         gameMap[y].append(1)
       else:
         gameMap[y].append(0)
   return gameMap
 
-def initSnake():
+def initSnake(width, height):
   snake = []
-  snake.append({ 'x': 11, 'y': 5 });
-  snake.append({ 'x': 11, 'y': 6 });
-  snake.append({ 'x': 11, 'y': 7 });
+  initX = int(width / 2) + 1
+  initY = int(height / 2)
+  snake.append({ 'x': initX, 'y': initY });
+  snake.append({ 'x': initX, 'y': initY + 1 });
+  snake.append({ 'x': initX, 'y': initY + 2 });
   return snake
 
 def xyIsInBody(
@@ -38,15 +41,20 @@ def xyIsInBody(
       break
   return isInBody
 
+def xyIsWall(x, y, width, height):
+  return x == 0 or x == width - 1 or y == 0 or y == height - 1
+
 def putFood(
   gameMap,
   snake,
+  weight,
+  height,
 ):
   x = 0
   y = 0
   while True:
-    x = 1 + int(random() * 18)
-    y = 1 + int(random() * 8)
+    x = 1 + int(random() * weight - 2)
+    y = 1 + int(random() * height - 2)
     if not xyIsInBody(x, y, snake):
       break
   gameMap[y][x] = 2
@@ -75,8 +83,8 @@ def render(
 
 while True:
   os.system('clear')
-  gameMap = initGameMap(20, 10)
-  snake = initSnake()
-  putFood(gameMap, snake)
+  gameMap = initGameMap(WEIGHT, HEIGHT)
+  snake = initSnake(WEIGHT, HEIGHT)
+  putFood(gameMap, snake, WEIGHT, HEIGHT)
   render(gameMap, snake)
   time.sleep(0.2)
