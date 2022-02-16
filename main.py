f1b8b1406bad2ee68883dd406ad430b0dd17024c
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-16 12:57:23 +0800

    基本完成

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 #!/opt/homebrew/bin/python3
 
-
-from asyncio import sleep
-from random import random
 import os
-import time
+from random import random
 
-WEIGHT = 30
+WIDTH = 30
 HEIGHT = 13
+DIR_DICT = { 'w': 'UP', 's': 'DOWN', 'a': 'LEFT', 'd': 'RIGHT' }
 
-def initGameMap(weight, height):
+def initGameMap(snake):
   gameMap = [];
-  for y in range(0, height):
+  for y in range(0, HEIGHT):
     gameMap.append([])
-    for x in range(0, weight):
-      if xyIsWall(x, y, weight, height):
+    for x in range(0, WIDTH):
+      if xyIsInWall(x, y):
         gameMap[y].append(1)
       else:
         gameMap[y].append(0)
+  putFood(gameMap, snake)
   return gameMap
 
-def initSnake(width, height):
+def initSnake():
   snake = []
-  initX = int(width / 2) + 1
-  initY = int(height / 2)
+  initX = int(WIDTH / 2) + 1
+  initY = int(HEIGHT / 2)
   snake.append({ 'x': initX, 'y': initY });
   snake.append({ 'x': initX, 'y': initY + 1 });
   snake.append({ 'x': initX, 'y': initY + 2 });
@@ -41,8 +40,8 @@ def xyIsInBody(
       break
   return isInBody
 
-def xyIsWall(x, y, width, height):
-  return x == 0 or x == width - 1 or y == 0 or y == height - 1
+def xyIsInWall(x, y):
+  return x == 0 or x == WIDTH - 1 or y == 0 or y == HEIGHT - 1
 
 def xyIsFood(x, y, gameMap):
   return gameMap[y][x] == 2
@@ -50,53 +49,24 @@ def xyIsFood(x, y, gameMap):
 def clearFood(
   gameMap,
 ):
-  y = 0
-  x = 0
-  for row in gameMap:
-    for land in row:
-      if land == 2:
+  for y in range(0, HEIGHT):
+    for x in range(0, WIDTH):
+      if gameMap[y][x] == 2:
         gameMap[y][x] = 0
-      x += 1
-    y += 1
-      
 
 def putFood(
   gameMap,
   snake,
-  weight,
-  height,
 ):
-  # clearFood(gameMap)
+  clearFood(gameMap)
   x = 0
   y = 0
   while True:
-    x = 1 + int(random() * weight - 2)
-    y = 1 + int(random() * height - 2)
+    x = 1 + int(random() * (WIDTH - 2))
+    y = 1 + int(random() * (HEIGHT - 2))
     if not xyIsInBody(x, y, snake):
       break
   gameMap[y][x] = 2
-
-def render(
-  gameMap,
-  snake,
-):
-  y = 0
-  for row in gameMap:
-    x = 0
-    rowStr = ''
-    for land in row:
-      if xyIsInBody(x, y, snake):
-        rowStr += '●'
-      else:
-        if land == 0:
-          rowStr += ' '
-        elif land == 1:
-          rowStr += '■'
-        elif land == 2:
-          rowStr += '☆'
-      x += 1
-    print(rowStr)
-    y += 1
 
 def snakeMove(
   gameMap,
@@ -120,39 +90,55 @@ def snakeMove(
     targetX = head['x'] + 1
   if xyIsInBody(targetX, targetY, snake):
     return '撞自己'
-  if xyIsWall(targetX, targetY, WEIGHT, HEIGHT):
+  if xyIsInWall(targetX, targetY):
     return '撞墙'
-  newHead = {
-    'x': targetX,
-    'y': targetY,
-  }
+  newHead = { 'x': targetX, 'y': targetY }
   snake.insert(0, newHead)
   if xyIsFood(targetX, targetY, gameMap):
-    putFood(gameMap, snake, WEIGHT, HEIGHT)
+    putFood(gameMap, snake)
   else:
     snake.pop()
   return '平安'
 
-gameMap = initGameMap(WEIGHT, HEIGHT)
-snake = initSnake(WEIGHT, HEIGHT)
-putFood(gameMap, snake, WEIGHT, HEIGHT)
-direction = 'UP'
-dirDict = {
-  'w': 'UP',
-  's': 'DOWN',
-  'a': 'LEFT',
-  'd': 'RIGHT',
-}
-while True:
-  os.system('clear')
-  render(gameMap, snake)
+def render(
+  gameMap,
+  snake,
+):
+  y = 0
+  for row in gameMap:
+    x = 0
+    rowStr = ''
+    for land in row:
+      if xyIsInBody(x, y, snake):
+        rowStr += '●'
+      else:
+        if land == 0:
+          rowStr += ' '
+        elif land == 1:
+          rowStr += '■'
+        elif land == 2:
+          rowStr += '☆'
+      x += 1
+    print(rowStr)
+    y += 1
+
+def main():
+  direction = 'UP'
   status = '平安'
-  keyCode = input()
-  if keyCode in dirDict.keys():
-    direction = keyCode
-  elif keyCode == 'exis':
-    break
-  status = snakeMove(gameMap, snake, direction)
-  if status != '平安':
-    print(status)
-    break
+  snake = initSnake()
+  gameMap = initGameMap(snake)
+  while True:
+    os.system('clear')
+    render(gameMap, snake)
+    keyCode = input('请输入（w上，s下，a左，d右，enter继续，exit退出）：')
+    if keyCode in DIR_DICT.keys():
+      direction = DIR_DICT[keyCode]
+    elif keyCode == 'exit':
+      status = '用户退出'
+      break
+    status = snakeMove(gameMap, snake, direction)
+    if status != '平安':
+      break
+  print('游戏结束，原因：', status)  
+
+main()
